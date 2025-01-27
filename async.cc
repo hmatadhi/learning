522aac2dfef5c04f2c881d9a7010c257abd1da02
hmatadhi <harsha.matadhikari@gmail.com>
2025-01-27 14:34:54 +0530

    Update async.cc

--- async.cc
+++ async.cc
@@ -3,43 +3,61 @@
 #include <thread>
 #include <atomic>
 
-int g = 100;
+class Connection {
+    public:
+       Connection() { a = 200; } 
+       int a;
+};
+
+class asyncConnection {
+    public:
+        Connection *conn;
+};
+
+Connection g;
+
+Connection *get_live_connection()
+{
+    return &g;
+}
 
 // Modify asyncFunction to take the sleep duration in milliseconds
-int asyncFunction(int sleep_ms, std::atomic<bool>& cancel_flag) {
-    for (int i = 0; i < sleep_ms; ++i) {
+asyncConnection asyncFunction(std::atomic<bool>& cancel_flag) {
+    asyncConnection asyncConn;
+    asyncConn.conn = nullptr;
+    
+    for (int i = 0; i < 50; ++i) {
         if (cancel_flag.load()) {
             std::cout << "Async function cancelled." << std::endl;
-            return -1; // Indicate that the function was cancelled
+            return asyncConn; // Indicate that the function was cancelled
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
-    g = 200;
-    return 42;
+    asyncConn.conn = get_live_connection();
+    return asyncConn;
 }
 
 int main(int argc, char* argv[])
 {
     std::cout << "Async Test";
 
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <wait_time_ms> <sleep_ms>" << std::endl;
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <wait_time_ms>" << std::endl;
         return 1;
     }
 
     int wait_time_ms = std::stoi(argv[1]);
-    int sleep_ms = std::stoi(argv[2]);
+    //int sleep_ms = std::stoi(argv[2]);
 
     std::atomic<bool> cancel_flag(false);
-    std::future<int> result = std::async(std::launch::async, asyncFunction, sleep_ms, std::ref(cancel_flag));
+    std::future<asyncConnection> result = std::async(std::launch::async, asyncFunction, std::ref(cancel_flag));
     std::cout << "Waiting for async result..." << std::endl;
 
     if (result.wait_for(std::chrono::milliseconds(wait_time_ms)) == std::future_status::ready) {
-        int value = result.get();
-        std::cout << "Async result: " << value << " global value = " << g << std::endl;
+        asyncConnection value = result.get();
+        std::cout << "Async result: " << value.conn->a << std::endl;
     } else {
         cancel_flag.store(true); // Signal the async function to cancel
-        std::cout << "Async result not ready within the timeout period. global value g = " << g << std::endl;
     }
 
     return 0;
